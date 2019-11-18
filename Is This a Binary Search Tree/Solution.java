54cc170cc3f18c460dc9ef0a9b2d3c99f1624fff
Gayatri Ganapthy <30742345+Gayatri-2017@users.noreply.github.com>
2019-11-18 04:43:28 -0800

    Added problem link

--- Is This a Binary Search Tree/Solution.java
+++ Is This a Binary Search Tree/Solution.java
@@ -1,11 +1,14 @@
 /* Hidden stub code will pass a root argument to the function below. Complete the function to solve the challenge. Hint: you may want to write one or more helper functions.  
 
+https://www.hackerrank.com/challenges/is-binary-search-tree/problem
+
 The Node class is defined as follows:
     class Node {
         int data;
         Node left;
         Node right;
      }
+
 */
     boolean checkBST(Node root) {
         return myCheckTree(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
