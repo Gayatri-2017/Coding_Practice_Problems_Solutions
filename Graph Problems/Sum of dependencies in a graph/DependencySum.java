7940a8c25fedc83a7076fec6a76d2b3514dec99f
Gayatri Ganapthy <30742345+Gayatri-2017@users.noreply.github.com>
2019-12-22 15:16:27 -0800

    Update and rename DependencySum.java to Graph Problems/Sum of dependencies in a graph/DependencySum.java

--- Graph Problems/Sum of dependencies in a graph/DependencySum.java
+++ Graph Problems/Sum of dependencies in a graph/DependencySum.java
@@ -4,6 +4,9 @@
 Given a directed graph with n nodes. 
 If there is an edge from u to v then u depends on v. 
 Our task is to find out the sum of dependencies for every node.
+
+https://practice.geeksforgeeks.org/problems/sum-of-dependencies-in-a-graph/0
+
 */
 
 import java.util.*;
