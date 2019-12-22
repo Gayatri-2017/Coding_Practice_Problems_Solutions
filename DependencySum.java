ea839c52d68741603345128528085cd436f7c288
Gayatri Ganapthy <30742345+Gayatri-2017@users.noreply.github.com>
2019-12-22 15:09:08 -0800

    Add files via upload

---
+++ DependencySum.java
@@ -0,0 +1,80 @@
+/*package whatever //do not write package name here */
+
+/*
+Given a directed graph with n nodes. 
+If there is an edge from u to v then u depends on v. 
+Our task is to find out the sum of dependencies for every node.
+*/
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class DependencySum {
+	public static void main (String[] args) {
+		//code
+	    Scanner sc = new Scanner(System.in);
+	    int testCases = sc.nextInt();
+	    while(testCases > 0){
+			int n = sc.nextInt();
+			int e = sc.nextInt();
+			// System.out.println("n = " + n + " e = " + e);
+			Graph graph = new Graph(n);
+			// String inpStr = sc.next();
+			// System.out.println(inpStr);
+			// String inpArray[] = inpStr.split(" ");
+			// for (int i = 0; i < inpArray.length; i++) {
+			// 	System.out.println(Integer.parseInt(inpArray[i]));
+			// }
+			for (int i=0; i<e; i++) {
+				graph.addEdge(sc.nextInt(), sc.nextInt());
+			}
+			System.out.println(graph.findCount(0));
+			testCases--;
+	    }
+	}
+}
+
+class Graph{
+	int v;
+	LinkedList<Integer> adj[];
+	Graph(int v){		
+		this.v = v;
+		adj = new LinkedList[v];
+		for (int i = 0; i < v; i++) {
+			adj[i] = new LinkedList();	
+		}
+	}
+	void addEdge(int u, int v){
+		adj[u].add(v);
+	}
+	int findCount(int start){
+		// return 1;
+		int count = 0;
+		boolean visited[] = new boolean[v];
+		LinkedList<Integer> queue = new LinkedList<Integer>();
+
+		// visited[start] = true;
+		// queue.add(start);
+
+		// while(!queue.isEmpty()){
+		// 	int v = queue.poll();
+		for (int i=start; i<v; i++) {
+			
+
+			Iterator<Integer> li = adj[i].listIterator();
+			while(li.hasNext()){
+
+				int liVal = li.next();
+				// if(!visited[liVal]){
+					// visited[liVal] = true;
+					// queue.add(liVal);
+					// count++;
+				// }
+				count++;
+			}
+		}
+		return count;
+
+	}
+}
---
+++ PrintAdjacencyList.java
@@ -0,0 +1,93 @@
+/*package whatever //do not write package name here */
+/*
+Given number of edges 'E' and vertices 'V' of a bidirectional graph. 
+Your task is to build a graph through adjacency list and print the adjacency list for each vertex.
+*/
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class PrintAdjacencyList {
+	public static void main (String[] args) {
+		//code
+	    Scanner sc = new Scanner(System.in);
+	    int testCases = sc.nextInt();
+	    while(testCases > 0){
+			int n = sc.nextInt();
+			int e = sc.nextInt();
+			// System.out.println("n = " + n + " e = " + e);
+			Graph graph = new Graph(n);
+			// String inpStr = sc.next();
+			// System.out.println(inpStr);
+			// String inpArray[] = inpStr.split(" ");
+			// for (int i = 0; i < inpArray.length; i++) {
+			// 	System.out.println(Integer.parseInt(inpArray[i]));
+			// }
+			for (int i=0; i<e; i++) {
+				graph.addEdge(sc.nextInt(), sc.nextInt());
+			}
+			// System.out.println(graph.findCount(0));
+			graph.printAdj();
+			testCases--;
+	    }
+	}
+}
+
+class Graph{
+	int v;
+	LinkedList<Integer> adj[];
+	Graph(int v){		
+		this.v = v;
+		adj = new LinkedList[v];
+		for (int i = 0; i < v; i++) {
+			adj[i] = new LinkedList();	
+		}
+	}
+	void addEdge(int u, int v){
+		adj[u].add(v);
+		adj[v].add(u);
+	}
+	// int findCount(int start){
+	void printAdj(){
+		// return 1;
+		// int count = 0;
+		boolean visited[] = new boolean[v];
+		// String output = new String();
+		
+		LinkedList<Integer> queue = new LinkedList<Integer>();
+
+		// visited[start] = true;
+		// queue.add(start);
+
+		// while(!queue.isEmpty()){
+		// 	int v = queue.poll();
+		for (int i=0; i<v; i++) {
+			// output = "" + i;
+			ArrayList<Integer> outputList = new ArrayList<Integer>();
+			outputList.add(i);
+			Iterator<Integer> li = adj[i].listIterator();
+			while(li.hasNext()){
+				int liVal = li.next();
+				// output = output + liVal;
+				outputList.add(liVal);
+				// System.out.print(liVal + "->");
+				// if(!visited[liVal]){
+					// visited[liVal] = true;
+					// queue.add(liVal);
+					// count++;
+				// }
+				// count++;
+			}
+			// for (int j=0; j<output.length()-1; j++) {
+			// System.out.println("For i = " + i + " Size = " + outputList.size());
+			for (int j=0; j<outputList.size()-1; j++) {
+				// System.out.print(output.charAt(j) + "-> ");
+				System.out.print(outputList.get(j) + "-> ");
+			}
+			// System.out.println(output.charAt(output.length()-1));
+			System.out.println(outputList.get(outputList.size()-1));
+			// output = "";
+		}
+		// return count;
+	}
+}
