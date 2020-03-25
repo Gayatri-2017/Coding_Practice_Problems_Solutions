a0a63ec531886e218c76cea410006faef39a42cb
Gayatri Ganapthy <30742345+Gayatri-2017@users.noreply.github.com>
2020-03-24 23:59:49 -0700

    Create Solution.java

---
+++ Hackerrank/Repeated String/Solution.java
@@ -0,0 +1,58 @@
+/*
+Lilah has a string, s, of lowercase English letters that she repeated infinitely many times.
+Given an integer, n, find and print the number of letter a's in the first n letters of Lilah's infinite string.
+For example, if the string s='abcac' and n=10, the substring we consider is abcacabcac, the first 10 characters of her infinite string. There are 4 occurrences of a in the substring.
+
+https://www.hackerrank.com/challenges/repeated-string/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+
+*/
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the repeatedString function below.
+    static long repeatedString(String s, long n) {
+        int stringLength = s.length();
+        int totalCount = 0;
+        int partCount = 0;
+        long modVal = n%stringLength;
+        long quotVal = n/stringLength;
+
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i)=='a'){
+                totalCount++;
+                if(i<modVal){
+                    partCount++;
+                }
+            }
+        }
+        return(totalCount*quotVal+partCount);
+    }   
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = scanner.nextLine();
+
+        long n = scanner.nextLong();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        long result = repeatedString(s, n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
