9b7d732551f246def4d47e4a92cb13339b0aedbb
Gayatri Ganapthy <30742345+Gayatri-2017@users.noreply.github.com>
2021-02-18 20:40:48 -0800

    Create string_merge.py

---
+++ string_merge.py
@@ -0,0 +1,32 @@
+'''
+Write an immutable function that merges the following inputs into a single list.
+
+Inputs
+- Original list of strings
+- List of strings to be added
+- List of strings to be removed
+
+Return
+- List shall only contain unique values
+- List shall be ordered as follows
+--- Most character count to least character count
+--- In the event of a tie, reverse alphabetical
+'''
+
+def string_merge(orig_list, add_list, remove_list):
+	orig_list += [elem for elem in add_list if elem not in orig_list]
+
+	# Assumming values in the remove_list are present in the orig_list
+	orig_list = [elem for elem in orig_list if elem not in remove_list]
+
+	orig_list.sort(reverse=True)
+	orig_list.sort(key=len, reverse=True)
+
+	return orig_list
+
+# Driver code
+orig_list = ['one', 'two', 'three',]
+add_list = ['one', 'two', 'five', 'six']
+remove_list = ['two', 'five']
+
+print("Result List = \n", string_merge(orig_list, add_list, remove_list))
