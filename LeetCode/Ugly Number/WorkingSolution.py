a339e3d6893749e552a009200079d77552297df7
Gayatri Ganapathy <30742345+Gayatri-2017@users.noreply.github.com>
2021-05-10 16:41:21 -0700

    Create WorkingSolution.py

---
+++ LeetCode/Ugly Number/WorkingSolution.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/ugly-number/
+
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        
+        if n<=0:
+            return False
+        
+        while(n%5 == 0):
+            n /= 5
+        while(n%3 == 0):
+            n /= 3
+        while(n%2 == 0):
+            n /= 2
+        
+        if n == 1:
+            return True
+        else:
+            return False
+        
