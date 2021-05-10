48b5536529d04e174d49ac8369a3a4a3f8b2d550
Gayatri Ganapathy <30742345+Gayatri-2017@users.noreply.github.com>
2021-05-10 16:40:29 -0700

    Create Solution.py

---
+++ LeetCode/Ugly Number/Solution.py
@@ -0,0 +1,21 @@
+class Solution:
+  def isUgly(self, n: int) -> bool:
+    if n<=0:
+        return False
+    if n == 1:
+        return True
+
+    factors = []
+    sqrt_n = int(sqrt(n))
+    for i in range(1, sqrt_n+1):
+        if n%i == 0:
+            print("i = ", i)
+            factors.append(i)
+
+
+    for factor in factors:
+        if factor%2 != 0 and factor%3 != 0 and factor%5 != 0:
+            return False
+
+    return True
+        
