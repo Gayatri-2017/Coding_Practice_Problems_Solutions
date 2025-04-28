451a21f8bc8b007da22b851039bbdee6494a7cac
Gayatri Ganapathy <30742345+Gayatri-2017@users.noreply.github.com>
2025-04-28 16:52:26 -0700

    Create Solution2.py

---
+++ Neetcode/LRU Cache/Solution2.py
@@ -0,0 +1,39 @@
+class Node:
+    def __init__(self, key, val):
+        self.key, self.val = key, val
+        self.prev = self.next = None
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.cache = {}  # map key to node
+
+        self.lru, self.mru = Node(0, 0), Node(0, 0)
+        self.lru.next, self.mru.prev = self.mru, self.lru
+
+    def remove(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next, nxt.prev = nxt, prev
+
+    def insert(self, node):
+        prev, nxt = self.mru.prev, self.mru
+        prev.next = nxt.prev = node
+        node.next, node.prev = nxt, prev
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].val
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.remove(self.cache[key])
+        self.cache[key] = Node(key, value)
+        self.insert(self.cache[key])
+
+        if len(self.cache) > self.cap:
+            curr_lru = self.lru.next
+            self.remove(curr_lru)
+            del self.cache[curr_lru.key]
