4c7dabb438d2c67d405983405c5089eb8d7fc3fc
Gayatri Ganapathy <30742345+Gayatri-2017@users.noreply.github.com>
2025-04-27 21:52:11 -0700

    Create Solution2.py

---
+++ Neetcode/Valid Sudoku/Solution2.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = [0] * 9
+        cols = [0] * 9
+        squares = [0] * 9
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+                
+                val = int(board[r][c]) - 1
+                if (1 << val) & rows[r]:
+                    return False
+                if (1 << val) & cols[c]:
+                    return False
+                if (1 << val) & squares[(r // 3) * 3 + (c // 3)]:
+                    return False
+                    
+                rows[r] |= (1 << val)
+                cols[c] |= (1 << val)
+                squares[(r // 3) * 3 + (c // 3)] |= (1 << val)
+
+        return True
